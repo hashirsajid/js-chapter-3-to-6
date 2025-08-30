89c2834dc8603252a69b93fe3fd2533433686bc9
hashirsajid <muhammadhashir19731973@gmail.com>
2025-08-30 02:42:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Variables
+let studentName = "Ali";   
+// let 123name = "Error";  
+
+// Familiar Operators
+let sum = 5 + 3;       
+let product = 4 * 2;   
+
+// Unfamiliar Operators
+let a = 5;
+a++;   
+let b = 2 ** 4;  
